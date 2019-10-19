2801939dba1c86597a7d97de96282c5e921acf20
Kirill Kulakov <kirill578@gmail.com>
2019-10-18 19:22:52 -0500

    use material ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Box, Button } from '@material-ui/core'
 
-function App() {
+export const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box position="absolute" width="100%" height="100%" p="10px">
+      <Button variant="outlined">Button</Button>
+    </Box>
   );
 }
-
-export default App;
