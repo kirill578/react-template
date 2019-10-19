f941d3f9a9db97e68679857ee1c1148fb53c53bc
Kirill Kulakov <kirill578@gmail.com>
2019-10-18 19:33:03 -0500

    Updates

---
+++ precache-manifest.b872dc88afb2d0aa8971a76023c9641e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "797de24da51d91f1edd73d2e35442031",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a4b177eb203ab7404430",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "bebf8db974faa59d1f66",
+    "url": "/static/js/2.4a1b4e6d.chunk.js"
+  },
+  {
+    "revision": "a4b177eb203ab7404430",
+    "url": "/static/js/main.4a2af72c.chunk.js"
+  },
+  {
+    "revision": "2e4bc1b3c022e2e6153b",
+    "url": "/static/js/runtime-main.d54b910c.js"
+  }
+]);
